32e7a1a5ccf0b4d0144b721655e36ed836744c3e
mathugowshic <mathugowshic10@gmail.com>
2026-06-16 10:52:35 +0530

    feat: add upload router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
+from app.routers import upload
 
 app = FastAPI(
     title="AI Research Paper Assistant",
     version="1.0.0"
 )
 
+app.include_router(upload.router)
+
+
 @app.get("/")
-def home():
+async def home():
     return {
         "message": "AI Research Paper Assistant Backend Running"
     }
